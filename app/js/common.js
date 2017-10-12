0b40d78b27e2e1726bad2c3b8c1577a13087ba86
Petr Petukhov <kotov0991@gmail.com>
2017-10-13 01:17:53 +0300

    day2-slider

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
 $(function() {
 
-	// Custom JS
+	// adaptive menu
+	$(".toggle-menu").click(function() {
+		$(this).toggleClass("on");
+	});
+
+
+	//Slider
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		nav: false,
+		navText: '',
+		autoplay : false,
+		autoplayHoverPause : false,
+		loop: true
+	});
 
 });
